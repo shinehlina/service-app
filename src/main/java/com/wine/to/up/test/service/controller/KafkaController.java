99c516df13315d9cec97a36623f6c92e181b4f25
Ivan Zelenkov <vertvery@gmail.com>
2020-07-25 16:36:31 +0300

    add mapping

--- src/main/java/com/wine/to/up/test/service/controller/KafkaController.java
+++ src/main/java/com/wine/to/up/test/service/controller/KafkaController.java
@@ -1,15 +1,16 @@
 package com.wine.to.up.test.service.controller;
 
+import com.wine.to.up.test.api.dto.ServiceMessage;
 import com.wine.to.up.test.service.service.KafkaSendMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,16 +33,22 @@ public class KafkaController {
 
     @PostMapping(value = "/send/{topicName}")
     public void sendMessage(@PathVariable String topicName, @RequestParam String message) {
+        sendMessageWithHeaders(topicName, new ServiceMessage(Collections.emptyMap(), message));
+    }
 
+    @PostMapping(value = "/send/{topicName}/headers")
+    public void sendMessageWithHeaders(@PathVariable String topicName, @RequestParam @Valid ServiceMessage message) {
         AtomicInteger counter = new AtomicInteger(0);
         ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Headers headers = new RecordHeaders();
+        message.getHeaders().forEach(headers::add);
 
         int sent = Stream.iterate(1, v -> v + 1)
                 .limit(10)
                 .map(n -> executorService.submit(() -> {
                     int numOfMessages = 10;
                     for (int j = 0; j < numOfMessages; j++) {
-                        kafkaSendMessageService.sendMessage(topicName, null, message);
+                        kafkaSendMessageService.sendMessage(topicName, headers, message.getMessage());
                         counter.incrementAndGet();
                     }
                     return numOfMessages;
