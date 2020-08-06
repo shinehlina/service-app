f170a249cba0856db94c1f870f1b3e1b35663c1b
Ivan Zelenkov <ivan_zelenkov@dell.com>
2020-08-07 00:13:46 +0300

    fixes

---
+++ src/main/java/com/wine/to/up/service/kafka/TestTopicKafkaMessageHandler.java
@@ -0,0 +1,39 @@
+package com.wine.to.up.service.kafka;
+
+import com.wine.to.up.api.ServiceApiProperties;
+import com.wine.to.up.service.domain.entity.Message;
+import com.wine.to.up.service.repository.MessageRepository;
+import com.wine.to.up.test.api.message.KafkaServiceEventOuterClass.KafkaServiceEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@Slf4j
+public class TestTopicKafkaMessageHandler implements KafkaMessageHandler<String> {
+    private final MessageRepository messageRepository;
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    private final ServiceApiProperties serviceApiProperties;
+
+    @Autowired
+    public TestTopicKafkaMessageHandler(MessageRepository messageRepository, ServiceApiProperties serviceApiProperties) {
+        this.messageRepository = messageRepository;
+        this.serviceApiProperties = serviceApiProperties;
+        log.info("Handler for topic {} is creating...", serviceApiProperties.getTopicName());
+    }
+
+    @Override
+    public void handle(String message) {
+        counter.incrementAndGet();
+        log.info("Message received from test topic: test, number of messages: {}", counter.get());
+        messageRepository.save(new Message(message));
+    }
+
+    public void handle(KafkaServiceEvent kafkaServiceMessage) {
+        //todo
+    }
+}
